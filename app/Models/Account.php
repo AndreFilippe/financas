a282b5932e0cf878f5330dff67511478fe018a31
Andre Filippe <andre.filippe@example.com>
2026-04-29 15:00:51 -0300

    feat: add alphabetical global scope to Account and Category models

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -11,6 +11,13 @@ class Account extends Model
     use LogsActivity;
     protected $fillable = ['name', 'type', 'balance', 'is_benefit'];
 
+    protected static function booted()
+    {
+        static::addGlobalScope('alphabetical', function ($builder) {
+            $builder->orderBy('name', 'asc');
+        });
+    }
+
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = ['name', 'type', 'color'];
+    
+    protected static function booted()
+    {
+        static::addGlobalScope('alphabetical', function ($builder) {
+            $builder->orderBy('name', 'asc');
+        });
+    }
 
     public function transactions()
     {
